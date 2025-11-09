c1afcb48f51463f1c578b369ce1669219a97e6cd
Jonathan Hager <mail@hager-jonathan.de>
2025-11-09 17:59:19 +0100

    Renamed testbench dut

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_factory_test user_project (
+  tt_um_cejmu_wspr user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
