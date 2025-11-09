f3bcf06a8610fa2909d2034dfc6f6ebc77afa61d
Jonathan Hager <mail@hager-jonathan.de>
2025-11-09 11:05:18 +0100

    Initial commit for first submission

--- src/project.v
+++ src/project.v
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * tt_um_factory_test.v
+ *
+ * Test user module
+ *
+ * Author: Sylvain Munaut <tnt@246tNt.com>
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_factory_test (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +19,24 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uio_oe = 8'd255;
+    assign uio_out[7:1] = 7'b0000000;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    toplevel_uart toplevel (
+        .clk(clk),
+        .reset(rst_n),
+        .rx(ui_in[0]),
+        .start_transmission(ui_in[1]),
 
-endmodule
+        .encoding_valid(uo_out[0]),
+        .cos_ds(uo_out[1]),
+        .cos_ds_n(uo_out[2]),
+        .sin_ds(uo_out[3]),
+        .sin_ds_n(uo_out[4]),
+        .lo_i(uo_out[5]),
+        .lo_q(uo_out[6]),
+        .lo_ix(uo_out[7]),
+        .lo_qx(uio_out[0])
+    );
+
+endmodule  // tt_um_factory_test
